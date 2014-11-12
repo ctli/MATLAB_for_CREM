2aa103a2daff2fbc529f3bca80050fd8ec36e075
ctli <ctli@umich.edu>
2014-11-12 08:35:44 -0500

    update urban_c_o

--- process_urban_c_o.m
+++ process_urban_c_o.m
@@ -78,7 +78,7 @@ urb_o_n = sum(urb_o_g); % [1]x[6]
 urb_o_b = squeeze(sum(urb_o(:,end,:))); % biomass burning
 
 figure(400+i); clf; hold on; box on;
-hb0 = bar(yr-0.5, [urb_c_n; urb_o_n; urb_o_b']', 0.3, 'stacked', 'edge', 'none', 'linewidth', 2);
+hb0 = bar(yr-0.65, [urb_c_n; urb_o_n; urb_o_b']', 0.25, 'stacked', 'edge', 'none');
 % set(hb0(1), 'facec', [0.8 0 0]); %combustion
 % set(hb0(2), 'facec', [1 0.65 0]); %process
 % hb = bar(yr-0.5, [urb_c_n; urb_o_n; urb_o_b']', 0.3, 'stacked', 'edge', [0.5 0.5 0.5], 'linewidth', 2);
@@ -86,18 +86,19 @@ hb0 = bar(yr-0.5, [urb_c_n; urb_o_n; urb_o_b']', 0.3, 'stacked', 'edge', 'none',
 % set(hb(2), 'facec', [1 0.65 0]); %process
 % set(hb(3), 'facec', [1 1 0]); %biomass burning
 
-hb1 = bar(yr-0.5, sum([urb_c_n; urb_o_n; urb_o_b']), 0.3, 'stacked', 'facec', 'none', 'edge', [0.5 0.5 0.5], 'linewidth', 2);
+hb1 = bar(yr-0.65, sum([urb_c_n; urb_o_n; urb_o_b']), 0.25, 'stacked', 'facec', 'none', 'edge', [0.5 0.5 0.5], 'linewidth', 1);
 
 colormap copper
 set(hb0(1), 'facec', [0.5 0 0]); %biomass burning
 
 set(gca, 'fontsize', 10);
 set(gca, 'layer', 'top');
-set(gcf, 'unit', 'inch', 'pos', [0.25+4.15*(i-1)    4.25    4.0000    3.0000]);
+set(gcf, 'unit', 'inch', 'pos', [0.25+4.15*(i-1)    5.25    4.0000    3.0000]);
 set(gca, 'pos', [0.1458    0.1100    0.7722    0.8150]);
+set(gca, 'layer', 'bottom');
 ylabel([char(cellstr(urban_c_id{1}(i))), ' (Tg)'], 'fontsize', 12, 'fontweight', 'bold');
 xlim([2005 2031.5]);
-legend(fliplr(hb0), 'Biomass Burning', 'Industrial Process', 'Fossil Fuel Combustion', 2);
+legend(fliplr(hb0), 'Biomass Burning & Other', 'Industrial Process', 'Fossil Fuel Combustion', 2);
 set(legend, 'fontsize', 8);
 
 if i == 1, ylim([0 3]); end
@@ -109,6 +110,28 @@ if i == 6, ylim([0 55]); end
 if i == 7, ylim([0 40]); end
 if i == 8, ylim([0 70]); end
 if i == 9, ylim([0 65]); end
+end
+
+%%
+[urban_c, urban_c_id] = getgdx('result_egyint_n.gdx', 'urban_c'); % [urb]x[rs]x[g]x[t] = [9]x[30]x[8]x[6]
+[urban_o, urban_o_id] = getgdx('result_egyint_n.gdx', 'urban_o'); % [urb]x[rs]x[g]x[t] = [9]x[30]x[10]x[6]
+for i = 1:length(urban_c_id{1})
+urb_c = squeeze(urban_c(strcmp(urban_c_id{1}(i), urban_c_id{1}),:,:,:)); % [30]x[8]x[6]
+urb_c_g = squeeze(sum(urb_c,2)); % [30]x[6]
+urb_c_n = sum(urb_c_g); % [1]x[6]
+
+urb_o = squeeze(urban_o(strcmp(urban_o_id{1}(i), urban_o_id{1}),:,:,:)); % [30]x[8]x[6]
+urb_o_g = squeeze(sum(urb_o(:,1:end-1,:),2)); % [30]x[6]
+urb_o_n = sum(urb_o_g); % [1]x[6]
+urb_o_b = squeeze(sum(urb_o(:,end,:))); % biomass burning
+
+figure(400+i); 
+hb0 = bar(yr+0, [urb_c_n; urb_o_n; urb_o_b']', 0.25, 'stacked', 'edge', 'none');
+set(hb0(1), 'facec', [0.5 0 0]); %biomass burning
+hb1 = bar(yr+0, sum([urb_c_n; urb_o_n; urb_o_b']), 0.25, 'stacked', 'facec', 'none', 'edge', [0 0.6 0], 'linewidth', 1);
+
+file_name = ['fig_', char(cellstr(urban_c_id{1}(i)))];
+% export_fig(file_name);
 end
 
 %%
@@ -125,20 +148,19 @@ urb_o_n = sum(urb_o_g); % [1]x[6]
 urb_o_b = squeeze(sum(urb_o(:,end,:))); % biomass burning
 
 figure(400+i); 
-hb0 = bar(yr+0.5, [urb_c_n; urb_o_n; urb_o_b']', 0.3, 'stacked', 'edge', 'none', 'linewidth', 2);
+hb0 = bar(yr+0.65, [urb_c_n; urb_o_n; urb_o_b']', 0.25, 'stacked', 'edge', 'none');
 set(hb0(1), 'facec', [0.5 0 0]); %biomass burning
-hb1 = bar(yr+0.5, sum([urb_c_n; urb_o_n; urb_o_b']), 0.3, 'stacked', 'facec', 'none', 'edge', 'b', 'linewidth', 2);
+hb1 = bar(yr+0.65, sum([urb_c_n; urb_o_n; urb_o_b']), 0.25, 'stacked', 'facec', 'none', 'edge', 'b', 'linewidth', 1);
 
 if i == 1
-    text(2010, 3.1, 'Gray: No Policy; ', 'fontsize', 8, 'color', [0.5 0.5 0.5]);
-    text(2017.5, 3.1, 'Blue: Rgnl CO2 Cap', 'fontsize', 8, 'color', 'b');
+    text(2002.1, 3.15, 'Gray: No Policy; ', 'fontsize', 8, 'color', [0.5 0.5 0.5]);
+    text(2009.3, 3.15, 'Green: Ntnl Energy Intensity Target; ', 'fontsize', 8, 'color', [0 0.6 0]);
+    text(2025, 3.15, 'Blue: Rgnl CO2 Cap', 'fontsize', 8, 'color', 'b');
 end
 
 file_name = ['fig_', char(cellstr(urban_c_id{1}(i)))];
-% export_fig(file_name);
+export_fig(file_name, '-painters');
 end
-
-
 
 
 
